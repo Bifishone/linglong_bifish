032626ecd8c6fccd609a0ed8350574889d587dc7
辉小鱼 <111103537+Bifishone@users.noreply.github.com>
2025-09-04 16:49:02 +0800

    Update setting.go

--- global/setting.go
+++ global/setting.go
@@ -1,10 +1,24 @@
 package global
 
 import "linglong/pkg/setting"
+import "time"
 
 var (
-	ServerSetting *setting.ServerSettingS
+	ServerSetting *ServerSettingS
+	JWTSetting    *JWTSettingS
 	AppSetting      *setting.AppSettingS
 	DatabaseSetting *setting.DatabaseSettingS
 	MasscanSetting *setting.MasscanSettingS
 )
+
+type ServerSettingS struct {
+	RunMode      string
+	HttpPort     int
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+type JWTSettingS struct {
+	Secret string
+	Expire time.Duration
+}
